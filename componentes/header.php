ccc79369b6905c87ad5fff51641072f4709f0de6
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-06-24 09:33:17 -0300

    header feito e começo do dashboard

--- componentes/header.php
+++ componentes/header.php
@@ -1,5 +1,6 @@
 <?php 
-    include '../conexao.php';
+    $pagina = basename($_SERVER['PHP_SELF']);
+    include 'conexao.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,20 +9,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
     <header>
         <nav>
             <div id="navbar-superior">
-                <img src="../IMG/hamburger.png" id="IMGHEADER" alt="">
-                <span class="txt">Painel da Empresa</span>
+                <img src="IMG/hamburger.png" id="Hamburger" alt="">
+                <span id="txt-painel" class="txt">Painel da Empresa</span>
                 <input type="search" placeholder="Pesquisar" name="" id="searchbar">
-                <span class="txt">Admin</span>
-                <img src="../IMG/user.png" id="imguser" alt="">
+                <span id="txt-user">Admin</span>
+                <img src="IMG/user.png" id="imguser" alt="">
             </div>
             <ul id="navbar-links">
-                <li class="list"><div class="links" id="firstdiv"><a href="../index.php" class="anchor">DashBoard</a></div></li>
+                <li class="list"><div class="links" id="firstdiv"><a href="index.php" class="anchor">DashBoard</a></div></li>
                 <li class="list"><div class="links"><a href="produtos.php" class="anchor">Produtos</a></div></li>
                 <li class="list"><div class="links"><a href="funcionarios.php" class="anchor">Funcionarios</a></div></li>
                 <li class="list"><div class="links"><a href="clientes.php" class="anchor">Clientes</a></div></li>
--- index.php
+++ index.php
@@ -1,13 +1,33 @@
+<?php 
+    $ContalinhasFuncionarios = "SELECT SUM(FuncionarioID) AS FuncionariosContados FROM funcionarios";
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
     <?php
         include 'componentes/header.php';
     ?>
+    <main class="container">
+        <div id="div-txt">
+            <h1 class="txt" id="titulo-dashboard">Painel da Empresa</h1>
+            <span class="txt">Resumo Geral das consultas</span>
+        </div>
+        <section class="ContainerResultados">
+            <span></span>
+            <span></span>
+        </section>
+        <section class="ContainerResultados"></section>
+        <section class="ContainerResultados"></section>
+        <section class="ContainerResultados"></section>
+        <section class="ContainerResultados"></section>
+    </main>
 </body>
 </html>
